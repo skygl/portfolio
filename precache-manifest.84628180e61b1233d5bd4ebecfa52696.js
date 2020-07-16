a20fae32e10c177f5d6d5cfbd2f78b44107bc7de
skygl <jupiter9501@gmail.com>
2020-07-17 00:19:41 +0900

    Updates

---
+++ precache-manifest.84628180e61b1233d5bd4ebecfa52696.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d50a1fd2ab51a65bd59e",
+    "url": "/portfolio/static/css/main.1b9bbf5a.chunk.css"
+  },
+  {
+    "revision": "d50a1fd2ab51a65bd59e",
+    "url": "/portfolio/static/js/main.d50a1fd2.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "213c2fd0a0b5a4ad3e9af5a8f6f81ba3",
+    "url": "/portfolio/index.html"
+  }
+];
